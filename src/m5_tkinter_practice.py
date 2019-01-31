8eacc1d3672953e21c0681c9344b30f0e4a9f992
mdhummel076 <mdhummel076@gmail.com>
2019-01-31 08:40:13 -0500

    Done

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,8 +3,8 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Matt Hummel.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -13,28 +13,60 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # -------------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
 
+    window = tkinter.Tk()
+
+
     # -------------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
 
+    frame = ttk.Frame(window)
+    frame.grid()
+
+
     # -------------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
 
+    button = ttk.Button(frame)
+    button['command'] = lambda: print('Hello')
+    button.grid(row=0,column=0)
+
+    box = ttk.Entry(frame)
+    box.grid(row=0,column = 1)
+
+    button2 = ttk.Button(frame)
+    button2['command'] = lambda: printstuff(box)
+    button2.grid(row=1,column=1)
+
+    box2 = ttk.Entry(frame)
+    box2.grid(row=0,column=2)
+
+    button3 = ttk.Button(frame)
+    button3['command'] = lambda: print2(box,box2)
+    button3.grid(row = 1,column=2)
+
+    button4 = ttk.Button(frame,text = 'I move')
+    button4['command'] = lambda: button4.grid(row = 4, column = 2)
+    button4.grid(row = 2, column = 0)
+
+    window.mainloop()
+
+
     # -------------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
 
     # -------------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
@@ -43,7 +75,7 @@ def main():
     # -------------------------------------------------------------------------
 
     # -------------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -67,8 +99,21 @@ def main():
     ####################################################################
 
     # -------------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # DONE: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
+
+def printstuff(box):
+
+    if(box.get() == 'OK'):
+        print('Hello')
+    else:
+        print('Goodbye')
+
+def print2(box,box2):
+
+    n = int(box2.get())
+    for k in range(n):
+        print(box.get())
 
 
 # -----------------------------------------------------------------------------
